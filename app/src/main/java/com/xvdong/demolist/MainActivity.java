929fa7710dc85bf5b51bdd0f751db445295b11c8
wangzhongman <185514269@qq.com>
2022-09-05 13:30:00 +0800

    Create Demo

---
+++ app/src/main/java/com/xvdong/demolist/MainActivity.java
@@ -0,0 +1,14 @@
+package com.xvdong.demolist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
