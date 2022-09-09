6e3c208a352f2ea0ec620257ac4348e19f69183b
wangzhongman <185514269@qq.com>
2022-09-09 13:34:02 +0800

    MainActivity作为launcherActivity

--- app/src/main/java/com/xvdong/demolist/MainActivity.java
+++ app/src/main/java/com/xvdong/demolist/MainActivity.java
@@ -23,6 +23,5 @@ public class MainActivity extends AppCompatActivity {
 
     public void toRecycler(View view) {
         startActivity(new Intent(this, RecyclerViewDemoActivity.class));
-
     }
 }
