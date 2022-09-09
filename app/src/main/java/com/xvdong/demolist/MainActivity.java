0d143f92b97be7e0a1896362094c2a3b16eadc53
wangzhongman <185514269@qq.com>
2022-09-09 13:22:42 +0800

    MainActivity作为launcherActivity

--- app/src/main/java/com/xvdong/demolist/MainActivity.java
+++ app/src/main/java/com/xvdong/demolist/MainActivity.java
@@ -1,8 +1,13 @@
 package com.xvdong.demolist;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.xvdong.demolist.business.coordinator.activity.CoordinatorActivity;
+import com.xvdong.demolist.business.recycler.RecyclerViewDemoActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void toCoordinator(View view) {
+        startActivity(new Intent(this, CoordinatorActivity.class));
+    }
+
+    public void toRecycler(View view) {
+        startActivity(new Intent(this, RecyclerViewDemoActivity.class));
     }
 }
