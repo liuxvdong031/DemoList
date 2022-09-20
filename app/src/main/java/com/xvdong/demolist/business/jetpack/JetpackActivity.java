780b82368cd32c557ae69740e3faff3a4021fe6a
wangzhongman <185514269@qq.com>
2022-09-20 17:06:16 +0800

    RxJava 测试

--- app/src/main/java/com/xvdong/demolist/business/jetpack/JetpackActivity.java
+++ app/src/main/java/com/xvdong/demolist/business/jetpack/JetpackActivity.java
@@ -2,13 +2,20 @@ package com.xvdong.demolist.business.jetpack;
 
 import android.os.Bundle;
 
+import com.blankj.utilcode.util.LogUtils;
 import com.xvdong.demolist.R;
 import com.xvdong.demolist.business.jetpack.viewmodel.JetpackViewModel;
 import com.xvdong.demolist.databinding.ActivityJetpackBinding;
 
+import java.util.Random;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProvider;
+import io.reactivex.Observable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
+import io.reactivex.schedulers.Schedulers;
 
 public class JetpackActivity extends AppCompatActivity {
 
@@ -18,15 +25,91 @@ public class JetpackActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //Activity跟ViewModel产生关系的方式
+        //ViewModel Activity跟ViewModel产生关系的方式
         mViewModel = new ViewModelProvider(this).get(JetpackViewModel.class);
+
+        //DataBinding xml文件的数据绑定
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_jetpack);
         mBinding.setViewModel(mViewModel);
 
-        //Activity跟LifeCycle产生关系的方式
+        //Lifecycle让view 用于Activity的生命周期感应
         getLifecycle().addObserver(mBinding.lctv);
 
         mViewModel.getRepoList();
+
+        for (int i = 0; i < 20; i++) {
+            UserBean userBean = new UserBean(String.valueOf(i), i);
+            test(userBean);
+        }
+
     }
 
+    public void test (UserBean bean){
+//        LogUtils.json(bean);
+        if (true){
+            Observable.just(bean)
+                    .subscribeOn(Schedulers.io())
+                    .map(new Function<UserBean, UserBean>() {
+                        @Override
+                        public UserBean apply(UserBean userBean) throws Exception {
+                            Random random = new Random();
+                            Thread.sleep(random.nextInt(100000));
+                            userBean.setNum(userBean.getAge() * 10);
+                            if (userBean.age % 2== 0){
+                                throw new  RuntimeException("111");
+                            }
+//                            LogUtils.json("TAG",bean);
+                            return userBean;
+                        }
+                    }).subscribe(new Consumer<UserBean>() {
+                        @Override
+                        public void accept(UserBean userBean) throws Exception {
+                            LogUtils.json(userBean.hashCode());
+                            LogUtils.json(bean.hashCode());
+                        }
+                    }, new Consumer<Throwable>() {
+                        @Override
+                        public void accept(Throwable throwable) throws Exception {
+                            LogUtils.json(throwable);
+                        }
+                    });
+        }
+
+    }
+
+    public class UserBean {
+        private String name;
+        private int age;
+        private int num;
+
+        public String getName() {
+            return name == null ? "" : name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        public int getNum() {
+            return num;
+        }
+
+        public void setNum(int num) {
+            this.num = num;
+        }
+
+        public UserBean(String name, int age) {
+            this.name = name;
+            this.age = age;
+
+        }
+    }
 }
